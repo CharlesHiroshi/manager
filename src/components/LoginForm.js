0a4ba8c9ded9d2b72285c836f7746febeff35ac1
Charles <charles@MacBook.local>
2017-06-25 01:56:43 +0900

    Add Labels, Input texts and Button in 'FormLogin.js'

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            label='E-mail'
+            placeholder='example@example.com'
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label='Password'
+            placeholder='Password'
+          />
+        </CardSection>
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
       </Card>
     );
   }
