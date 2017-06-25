af3cf21ca75bf7450dc0c793c3b2469bc9a64d96
Charles <charles@MacBook.local>
2017-06-25 10:28:42 +0900

    Implement Login Success and Fail for user

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -26,6 +27,7 @@ class LoginForm extends Component {
             placeholder='example@example.com'
             onChangeText={this.onEmailChanged.bind(this)}
             value={this.props.email}
+            autoCapitalize='none'
           />
         </CardSection>
         <CardSection>
@@ -37,6 +39,9 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -47,11 +52,17 @@ class LoginForm extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password
-  };
+const styles = StyleSheet.create({
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  }
+});
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error } = auth;
+  return { email, password, error };
 };
 
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
