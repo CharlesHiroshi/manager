70fd85887faa7c5cb4f262ff4dc92477fbbdae01
Charles <charles@MacBook.local>
2017-06-25 13:46:55 +0900

    Create 'EmployeeCreate.js' and add to router

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key='auth'>
-        <Scene key='login' component={LoginForm} title='Please Login' />
+        <Scene 
+          key='login' 
+          component={LoginForm} 
+          title='Please Login' 
+        />
       </Scene>
       <Scene key='main'>
         <Scene 
-          onRight={() => console.log('OK')}
+          onRight={() => Actions.employeeCreate()}
           rightTitle='Add'
           key='employeeList' 
           component={EmployeeList} 
-          title='Employees' 
+          title='Employees'
+          initial 
+        />
+        <Scene 
+          key='employeeCreate'
+          component={EmployeeCreate}
+          title='Employee Create'
         />
       </Scene>
     </Router>
