40f5e12d95b6f24bf8af891f974998583193f2b9
Charles <charles@MacBook.local>
2017-06-25 13:32:26 +0900

    Add navButton in 'EmployeeList.js'

--- src/Router.js
+++ src/Router.js
@@ -9,7 +9,13 @@ const RouterComponent = () => (
         <Scene key='login' component={LoginForm} title='Please Login' />
       </Scene>
       <Scene key='main'>
-        <Scene key='employeeList' component={EmployeeList} title='Employees' />
+        <Scene 
+          onRight={() => console.log('OK')}
+          rightTitle='Add'
+          key='employeeList' 
+          component={EmployeeList} 
+          title='Employees' 
+        />
       </Scene>
     </Router>
 );
